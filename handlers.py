199ec907dcb6122492491718e09ba3a334348f6f
isBre <bresciadavide@outlook.it>
2024-04-19 16:26:35 +0200

    added comments and docstrings

--- handlers.py
+++ handlers.py
@@ -4,38 +4,67 @@ from typing import List, Tuple, Dict
 
 
 class TurnHandler:
-    def __init__(self, all_players : List[Player]) -> None:
-        self.counter = 0 # Incremental value of game turns
-        self.position = len(all_players) - 1 # Turn Position in [0, len(all_players)]
-        self.streak = 0 # Number of turns without doubts
+    """Manages the turns of the game."""
+    def __init__(self, all_players: List[Player]) -> None:
+        """
+        Initialize a TurnHandler object.
+
+        Parameters:
+        - all_players (List[Player]): A list of all players participating in the game.
+        """
+        self.counter = 0  # Incremental value of game turns
+        self.position = len(all_players) - 1  # Turn Position in [0, len(all_players)]
+        self.streak = 0  # Number of turns without doubts
 
 class PlayersHandler:
-    def __init__(self, all_players : List[Player]) -> None:
-        self.all = all_players # All players (currently playing and not)
-        self.playing = all_players.copy() # Players that are playing 
+    """Manages the players in the game."""
+    def __init__(self, all_players: List[Player]) -> None:
+        """
+        Initialize a PlayersHandler object.
+
+        Parameters:
+        - all_players (List[Player]): A list of all players participating in the game.
+        """
+        self.all = all_players  # All players (currently playing and not)
+        self.playing = all_players.copy()  # Players that are playing 
         # (this is useful because if somebody wins it is removed from the game)
-        self.winners = [] # Winning players list
-        self.prev = None # The Player that preceed this player
-        self.this = None # The Player we need to consider
-        self.next = None # The Player next to this player
+        self.winners = []  # Winning players list
+        self.prev = None  # The Player that preceed this player
+        self.this = None  # The Player we need to consider
+        self.next = None  # The Player next to this player
 
 class BoardHandler:
-    def __init__(self, deck_size : int) -> None:
-        self.cards = [] # Cards within the board
-        self.number = 0 # the card number called from the previous player (0 means you're the first) 
-        self.availables = list(range(1, deck_size)) # The number of cards that are not discarded yet
-        self.latests = [] # Latest cards placed by the last player
+    """Manages the cards on the board."""
+    def __init__(self, deck_size: int) -> None:
+        """
+        Initialize a BoardHandler object.
+
+        Parameters:
+        - deck_size (int): The size of the deck of cards.
+        """
+        self.cards = []  # Cards within the board
+        self.number = 0  # the card number called from the previous player (0 means you're the first) 
+        self.availables = list(range(1, deck_size))  # The number of cards that are not discarded yet
+        self.latests = []  # Latest cards placed by the last player
         
 
 class GameHandler:
+    """Manages the overall game."""
     def __init__(
         self, 
         all_players: List[Player],
-        deck_size : int,
+        deck_size: int,
     ) -> None:
-        self.turn = TurnHandler(all_players = all_players)
-        self.players = PlayersHandler(all_players = all_players)
-        self.board = BoardHandler(deck_size = deck_size)
+        """
+        Initialize a GameHandler object.
+
+        Parameters:
+        - all_players (List[Player]): A list of all players participating in the game.
+        - deck_size (int): The size of the deck of cards.
+        """
+        self.turn = TurnHandler(all_players=all_players)
+        self.players = PlayersHandler(all_players=all_players)
+        self.board = BoardHandler(deck_size=deck_size)
         
     def n_playing_players(self) -> int:
         return len(self.players.playing)
@@ -115,19 +144,22 @@ class GameHandler:
 
  
 class StatsHandler:
-    # Keep track of mayor stats in order to make more precise decisions
-    
-    def __init__(self, 
-            all_players: List[Player],
-    ) -> None:
+    """Manages statistical data of players."""
+    def __init__(self, all_players: List[Player]) -> None:
+        """
+        Initialize a StatsHandler object.
+
+        Parameters:
+        - all_players (List[Player]): A list of all players participating in the game.
+        """
         self.data = {}
         for p in all_players:
             self.data[p.id] = {
-                'turns' : 0, # How many turns the player played
-                'not_first_turns' : 0, # How many turns the player played (not first hand)
-                'doubts' : 0, # Number of times the player doubted
-                'honest_times' : 0, # Number of times recorded that this player was honest when doubted
-                'dishonest_times' : 0, # number of times recorded that this player was dishonest when doubted
+                'turns': 0,  # How many turns the player played
+                'not_first_turns': 0,  # How many turns the player played (not first hand)
+                'doubts': 0,  # Number of times the player doubted
+                'honest_times': 0,  # Number of times recorded that this player was honest when doubted
+                'dishonest_times': 0,  # number of times recorded that this player was dishonest when doubted
             }
             
     def increase_turns_played(self, player : Player, first_hand : bool) -> None:
@@ -145,7 +177,18 @@ class StatsHandler:
         self.data[player.id]['dishonest_times'] += 1
 
 
-def generate_player_data(game_handler : GameHandler, stats_handler : StatsHandler) -> Dict:
+def generate_player_data(game_handler: GameHandler, stats_handler: StatsHandler) -> Dict:
+    """
+    Generate player data based on the current game state.
+    It's useful if you want to generate data for machine learning purpuses
+
+    Parameters:
+    - game_handler (GameHandler): The game handler object.
+    - stats_handler (StatsHandler): The stats handler object.
+
+    Returns:
+    - Dict: A dictionary containing various player data.
+    """
     data = {
         # Number of cards in the board (0 means you're the first)
         'board_cards': game_handler.n_cards_board(),
@@ -171,7 +214,14 @@ def generate_player_data(game_handler : GameHandler, stats_handler : StatsHandle
 
 class OutputPlayer:
     
-    def __init__(self, output : Dict) -> None:
+    """Represents the output of a player."""
+    def __init__(self, output: Dict) -> None:
+        """
+        Initialize an OutputPlayer object.
+
+        Parameters:
+        - output (Dict): Dictionary containing output information.
+        """
         self.output = output
         # TODO assert
     
